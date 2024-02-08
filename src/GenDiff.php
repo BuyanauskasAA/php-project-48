054825a43af04c87898a58d2c87b55451b3de341
BuyanauskasAA <Buyanauskas@gmail.com>
2024-02-08 19:46:25 +0600

    fix: adjust GenDiff.php

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -11,19 +11,21 @@ function genDiff(string $filepath1, string $filepath2): string
     sort($keys);
 
     $diffs = array_map(function ($key) use ($data1, $data2) {
+        $result = [];
         if (!array_key_exists($key, $data1)) {
-            return ['  + ' . $key . ': ' . var_export($data2[$key], true)];
+            $result[] = '  + ' . $key . ': ' . var_export($data2[$key], true);
         } elseif (!array_key_exists($key, $data2)) {
-            return ['  - ' . $key . ': ' . var_export($data1[$key], true)];
+            $result[] = '  - ' . $key . ': ' . var_export($data1[$key], true);
         } elseif ($data1[$key] !== $data2[$key]) {
-            return [
-                '  - ' . $key . ': ' . var_export($data1[$key], true),
-                '  + ' . $key . ': ' . var_export($data2[$key], true)
-            ];
+            $result[] = '  - ' . $key . ': ' . var_export($data1[$key], true);
+            $result[] = '  + ' . $key . ': ' . var_export($data2[$key], true);
         } else {
-            return ['    ' . $key . ': ' . var_export($data1[$key], true)];
+            $result[] = '    ' . $key . ': ' . var_export($data1[$key], true);
         }
+
+        return $result;
     }, $keys);
+
     $diffs = array_merge(['{'], ...$diffs);
     $diffs[] = '}';
     $diffString = implode("\n", $diffs);
