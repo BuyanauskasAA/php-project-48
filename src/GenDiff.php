8087c01dedf5ca0bb2af589b9b3c7156aa6ca603
BuyanauskasAA <Buyanauskas@gmail.com>
2024-02-08 19:35:48 +0600

    fix: adjust GenDiff.php

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -10,13 +10,16 @@ function genDiff(string $filepath1, string $filepath2): string
     $keys = array_keys([...$data1, ...$data2]);
     sort($keys);
 
-    $diffs = array_map(function ($key) use ($data1, $data2){
+    $diffs = array_map(function ($key) use ($data1, $data2) {
         if (!array_key_exists($key, $data1)) {
             return ['  + ' . $key . ': ' . var_export($data2[$key], true)];
         } elseif (!array_key_exists($key, $data2)) {
             return ['  - ' . $key . ': ' . var_export($data1[$key], true)];
         } elseif ($data1[$key] !== $data2[$key]) {
-            return ['  - ' . $key . ': ' . var_export($data1[$key], true), '  + ' . $key . ': ' . var_export($data2[$key], true)];
+            return [
+                '  - ' . $key . ': ' . var_export($data1[$key], true),
+                '  + ' . $key . ': ' . var_export($data2[$key], true)
+            ];
         } else {
             return ['    ' . $key . ': ' . var_export($data1[$key], true)];
         }
