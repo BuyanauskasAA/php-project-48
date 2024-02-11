fa0bc14c7ce507eeed914df7cec4d35d30365aba
BuyanauskasAA <Buyanauskas@gmail.com>
2024-02-11 18:33:38 +0600

    feat: update GenDiff.php

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -2,41 +2,52 @@
 
 namespace GenDiff;
 
-use function GenDiff\Utils\toString;
 use function GenDiff\Parser\parseFile;
 
-function genDiff(string $filepath1, string $filepath2): string
+function iter(array $data1, array $data2): array
+{
+    $keys = array_keys([...$data1, ...$data2]);
+    sort($keys);
+    return array_map(function ($key) use ($data1, $data2) {
+        if (!array_key_exists($key, $data1)) {
+            return [
+                'type' => 'added',
+                'key' => $key,
+                'value' => $data2[$key],
+            ];
+        } elseif (!array_key_exists($key, $data2)) {
+            return [
+                'type' => 'deleted',
+                'key' => $key,
+                'value' => $data1[$key],
+            ];
+        } elseif (is_array($data1[$key]) && is_array($data2[$key])) {
+            return [
+                'type' => 'nested',
+                'key' => $key,
+                'children' => iter($data1[$key], $data2[$key]),
+            ];
+        } elseif ($data1[$key] !== $data2[$key]) {
+            return [
+                'type' => 'changed',
+                'key' => $key,
+                'oldValue' => $data1[$key],
+                'newValue' => $data2[$key],
+            ];
+        } else {
+            return [
+                'type' => 'unchanged',
+                'key' => $key,
+                'value' => $data1[$key],
+            ];
+        }
+    }, $keys);
+}
+
+function genDiff(string $filepath1, string $filepath2): array
 {
     $data1 = parseFile($filepath1);
     $data2 = parseFile($filepath2);
 
-    $keys = array_keys([...$data1, ...$data2]);
-    sort($keys);
-
-    $diffs = array_map(function ($key) use ($data1, $data2) {
-        $result = [];
-        if (!array_key_exists($key, $data1)) {
-            $value = toString($data2[$key]);
-            $result[] = "  + {$key}: {$value}";
-        } elseif (!array_key_exists($key, $data2)) {
-            $value = toString($data1[$key]);
-            $result[] = "  - {$key}: {$value}";
-        } elseif ($data1[$key] !== $data2[$key]) {
-            $value1 = toString($data1[$key]);
-            $value2 = toString($data2[$key]);
-            $result[] = "  - {$key}: {$value1}";
-            $result[] = "  + {$key}: {$value2}";
-        } else {
-            $value = toString($data1[$key]);
-            $result[] = "    {$key}: {$value}";
-        }
-
-        return $result;
-    }, $keys);
-
-    $diffs = array_merge(['{'], ...$diffs);
-    $diffs[] = '}';
-    $diffString = implode("\n", $diffs);
-
-    return $diffString;
+    return iter($data1, $data2);
 }
