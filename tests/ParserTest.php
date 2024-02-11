d962fef595775bc15d4d2ff058203cbcf9e114c6
BuyanauskasAA <Buyanauskas@gmail.com>
2024-02-11 18:24:29 +0600

    feat: update ParserTest.php

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -8,22 +8,70 @@ use GenDiff\Parser;
 class ParserTest extends TestCase
 {
     private $expected1 = [
-        'host' => 'hexlet.io',
-        'timeout' => 50,
-        'proxy' => '123.234.53.22',
-        'follow' => false
+        "common" => [
+            "setting1" => "Value 1",
+            "setting2" => 200,
+            "setting3" => true,
+            "setting6" => [
+                "key" => "value",
+                "doge" => [
+                    "wow" => ""
+                ]
+            ]
+        ],
+        "group1" => [
+            "baz" => "bas",
+            "foo" => "bar",
+            "nest" => [
+                "key" => "value"
+            ]
+        ],
+        "group2" => [
+            "abc" => 12345,
+            "deep" => [
+                "id" => 45
+            ]
+        ]
     ];
 
     private $expected2 = [
-        'timeout' => 20,
-        'verbose' => true,
-        'host' => 'hexlet.io'
+        "common" => [
+            "follow" => false,
+            "setting1" => "Value 1",
+            "setting3" => null,
+            "setting4" => "blah blah",
+            "setting5" => [
+                "key5" => "value5"
+            ],
+            "setting6" => [
+                "key" => "value",
+                "ops" => "vops",
+                "doge" => [
+                    "wow" => "so much"
+                ]
+            ]
+        ],
+        "group1" => [
+            "foo" => "bar",
+            "baz" => "bars",
+            "nest" => "str"
+        ],
+        "group3" => [
+            "deep" => [
+                "id" => [
+                    "number" => 45
+                ]
+            ],
+            "fee" => 100500
+        ]
     ];
 
     public function testParseJson(): void
     {
         $actual1 = Parser\parseFile(__DIR__ . '/fixtures/file1.json');
+        $actual2 = Parser\parseFile(__DIR__ . '/fixtures/file2.json');
         $this->assertEquals($this->expected1, $actual1);
+        $this->assertEquals($this->expected2, $actual2);
     }
 
     public function testParseYaml(): void
