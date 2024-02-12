bfedc744eaaba0ecae15067c21131cdbf3f28875
BuyanauskasAA <Buyanauskas@gmail.com>
2024-02-12 12:33:39 +0600

    feat: add Stylish.php

---
+++ src/formatters/Stylish.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace GenDiff\Formatters\Stylish;
+
+function stringify(mixed $value, $depth): string
+{
+    $indentSize = 4;
+    $bracketIndent = str_repeat(' ', $depth * $indentSize);
+    $currentIndent = str_repeat(' ', ($depth + 1) * $indentSize);
+    switch (gettype($value)) {
+        case 'array':
+            $lines = array_map(function ($key) use ($value, $currentIndent, $depth) {
+                return "{$currentIndent}{$key}: " . stringify($value[$key], $depth + 1);
+            }, array_keys($value));
+            return implode("\n", array_merge(['{'], $lines, ["{$bracketIndent}}"]));
+
+        case 'boolean':
+            return $value ? 'true' : 'false';
+        case 'NULL':
+            return 'null';
+        default:
+            return (string) $value;
+    }
+}
+
+
+function makeStylish(array $diff, int $depth = 0): string
+{
+
+    $indentSize = 4;
+    $indent = str_repeat(' ', $depth * $indentSize);
+    $lines = array_map(function ($node) use ($depth, $indent) {
+        ['type' => $type, 'key' => $key] = $node;
+        return match ($type) {
+            'added' => "{$indent}  + {$key}: " . stringify($node['value'], $depth + 1),
+            'deleted' => "{$indent}  - {$key}: " . stringify($node['value'], $depth + 1),
+            'changed' =>
+            "{$indent}  - {$key}: " . stringify($node['oldValue'], $depth + 1)
+                . "\n" .
+                "{$indent}  + {$key}: " . stringify($node['newValue'], $depth + 1),
+            'unchanged' => "{$indent}    {$key}: " . stringify($node['value'], $depth + 1),
+            'nested' => "{$indent}    {$key}: " . makeStylish($node['children'], $depth + 1)
+        };
+    }, $diff);
+
+    return implode("\n", array_merge(['{'], $lines, ["{$indent}}"]));
+}
