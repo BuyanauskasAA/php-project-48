486f0d5c253c93b6f55c5f9b308e8b299a5fa5f3
BuyanauskasAA <Buyanauskas@gmail.com>
2024-02-17 12:28:32 +0600

    refactor: adjust Parser.php

--- src/Parser.php
+++ src/Parser.php
@@ -7,9 +7,10 @@ use Symfony\Component\Yaml\Yaml;
 function parseFile(string $filepath): array
 {
     $extension = pathinfo($filepath, PATHINFO_EXTENSION);
+    $content = file_get_contents($filepath) === false ? "" : file_get_contents($filepath);
     return match ($extension) {
-        'json' => json_decode(file_get_contents($filepath) ? file_get_contents($filepath) : '', true),
-        'yaml', 'yml' => Yaml::parseFile($filepath),
+        'json' => json_decode($content, true),
+        'yaml', 'yml' => Yaml::parse($content),
         default => "Wrong format {$extension}"
     };
 }
